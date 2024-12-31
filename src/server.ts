8b0aced54ca342e8b80b0fdf0ad35b82aebaeda7
Divyam1202 <chandakdivyam@gmail.com>
2024-12-31 10:57:00 +0530

    Update CORS origin in server.ts to new frontend URL

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://lmsxport-frontend-git-main-dcviits-projects.vercel.app",
+    origin: "https://lmsxport-frontend.vercel.app/",
     credentials: true,
   })
 );
